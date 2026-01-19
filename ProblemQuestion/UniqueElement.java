8bc9d64b786feac4574aa12fef2e86971aa95da8
AbdulxSikandar <abdulxsikandar@gmail.com>
2026-01-19 13:19:02 +0530

    Problem : Unique element from array

---
+++ ProblemQuestion/UniqueElement.java
@@ -0,0 +1,32 @@
+package ProblemQuestion;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class UniqueElement {
+    public static void main(String[] args) {
+        int [] a = { 1,2,3,4,5,6,7,3,1,2,5,6,4,7,15};
+        UniqueElement un = new UniqueElement();
+        un.uniqueelementarray(a);
+    }
+    
+    public void uniqueelementarray(int [] x){
+        Map<Integer, Integer> m1 = new HashMap<>();
+        for ( int arr : x) {
+            if (m1.containsKey(arr)){
+                m1.put(arr, m1.get(arr)+1);
+            }else{
+
+                m1.put(arr, 1);
+            }
+        }
+        for(int b : m1.keySet() ){
+            if(m1.get(b)>1){
+                continue;
+            }else{
+                System.out.println("Here is the Unique element from Array");
+                System.out.print( b + " ");
+            }
+        }
+    }
+}
