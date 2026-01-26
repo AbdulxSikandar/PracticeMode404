7ebf11b00757e18291f26c517058ff4936e9d94b
AbdulxSikandar <abdulxsikandar@gmail.com>
2026-01-26 15:07:24 +0530

    Problem : Minimum increment by k operations to make all equal

---
+++ ProblemQuestion/Minimumincrement.java
@@ -0,0 +1,40 @@
+package ProblemQuestion;
+
+public class Minimumincrement {
+
+    public static void main(String[] args) {
+        int arr[] = { 21, 33, 9, 45, 63 };
+        int  k = 3 ;
+        findMinimumincrementnumbers(arr, k);
+    }
+    
+    public static void findMinimumincrementnumbers(int[] x,int k){
+
+        if(x==null || x.length == 0){
+            return;
+        }
+
+        int n = x.length;
+        int numberofOperations = 0;
+
+        int max =x[0];
+        for(int m = 1; m<n;m++){
+            if(x[m]>max){
+                max = x[m];
+            }
+        }
+        boolean setincreament = false;
+        for(int i = 0; i<n ; i++){
+            if((max-x[i])%k==0){
+                numberofOperations+= (max-x[i])/k;
+                setincreament = true;
+            }else{
+                setincreament = false;
+                numberofOperations = -1;
+                break;
+            }
+        }
+        System.out.println(numberofOperations);
+
+    }
+}
