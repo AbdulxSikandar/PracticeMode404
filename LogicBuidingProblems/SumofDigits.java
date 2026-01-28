432b2a02d59539f6debf4ed509e391f87ea78343
AbdulxSikandar <abdulxsikandar@gmail.com>
2026-01-28 17:25:42 +0530

    Problem : Sum of digits

--- LogicBuidingProblems/SumofDigits.java
+++ LogicBuidingProblems/SumofDigits.java
@@ -1,5 +1,28 @@
 package LogicBuidingProblems;
-
 public class SumofDigits {
+    public static void main(String[] args) {
+        System.out.println(digitsSum(123786));
+        digitSum(2345);
+    }
+
+    static int digitsSum(int n){
+        String num = String.valueOf(n);
+        int sum =0;
+        for(int i =0;i<num.length();i++){
+            char chars = num.charAt(i);
+            int x = Integer.parseInt(String.valueOf(chars));
+            sum += x;
+        }
+        return sum;
+    }
+    
+    static void digitSum(int x){
+        int sum =0;
+        while(x != 0){
+            sum += x % 10;
+            x = x / 10;
+        }
+        System.out.println(sum);
+    }   
     
 }
