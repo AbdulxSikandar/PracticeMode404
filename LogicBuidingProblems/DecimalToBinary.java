bd9c109e95abb639f37cce8250341f586b60da8e
AbdulxSikandar <abdulxsikandar@gmail.com>
2026-01-31 22:27:23 +0530

    Problem : Convert the given number into an equivalent binary

---
+++ LogicBuidingProblems/DecimalToBinary.java
@@ -0,0 +1,23 @@
+package LogicBuidingProblems;
+
+public class DecimalToBinary{
+    public static void main(String[] args) {
+        System.out.println(dectoBinary(12));
+        
+    }
+    static String dectoBinary(int n){
+
+        StringBuilder binary = new StringBuilder();
+
+        while (n>0){
+            int bit = n%2;
+            binary.append((char)('0'+ bit));
+            n /=2;
+        }
+
+        // reverse the string
+        binary.reverse();
+        return binary.toString();
+
+    }
+}
