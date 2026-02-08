acd3ccb798ff785cb2fb7a0437c66e57fdb973bd
AbdulxSikandar <abdulxsikandar@gmail.com>
2026-02-08 09:54:08 +0530

    Problem : Container With Most Water and Arthmatic Progression from Sequence

--- TwoPointers/ContainerWithMost.java
+++ TwoPointers/ContainerWithMost.java
@@ -4,25 +4,50 @@ public class ContainerWithMost {
     public static void main(String[] args) {
         int[] arr = {1,2,3,6,8,9,10};
 
-        calculateWater(arr);
+        calculateWater2(arr);
     }
 
     //if array is Sorted
-    static void calculateWater(int[] x){
+    static void calculateWater1(int[] x){
 
         int maxwater = 0;
         int a = 0;
         int b = x.length-1;
         while (a<b) {
-            int width = x[b]-x[a];
+            int width = b-a;
             int water = width * Math.min(x[a],x[b]);
-            if(maxwater<water){
-                maxwater = water;
+            maxwater = Math.max(water, maxwater);
+
+            // move smaller height pointer
+            if (x[a] < x[b]) {
+                a++;        
+            } else {
+                b--;      
             }
-            a++;
         }
         System.out.println(maxwater);
 
         
+    }
+
+
+    static void calculateWater2(int [] x){
+
+        int water = 0;
+
+        for(int i =0; i<x.length;i++){
+            for(int j = i+1;j<x.length;j++){
+                int width = j-i;
+                int height = Math.min(x[i], x[j]);
+                int area = width*height;
+
+                if(area > water){
+                    water = area;
+                }
+
+            }
+        }
+
+        System.out.println("Max Water in Container : "+water);
     }
 }
